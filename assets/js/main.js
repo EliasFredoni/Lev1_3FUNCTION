f43e2941d532031aa043b3c54ee708ad0b03abad
EliasFredoni <fredoni.elias@gmail.com>
2022-04-14 14:20:03 +0200

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function intro3() {
+    console.log(" Hallo, mein Name ist " + superCoder + " ich bin " + alter + " und komme aus " + stadt)
+}
+
+let = superCoder = "Elias";
+let = stadt = "Wuppertal";
+let = alter = 37;
+
+intro3();
